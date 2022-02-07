4c0893dea2ada757e00d7370cf0e64bc7ab79a0d
Novya Eka Ningroom Juana <99195707+novyaenj@users.noreply.github.com>
2022-02-07 22:00:27 +0700

    Menghitung Banyaknya Bilangan Prima

---
+++ bilanganprima.py
@@ -0,0 +1,21 @@
+from itertools import count
+
+
+def is_prima (x):
+    for i in range(2,x):
+        if x % i == 0:
+            return False
+
+    return True
+
+def cari_bilangan_prima(awal,akhir):
+    list_bilangan_prima = []
+
+    for x in range(awal,akhir + 1):
+        if is_prima(x):
+            list_bilangan_prima.append(x)
+    return list_bilangan_prima
+
+data = len(cari_bilangan_prima(2,10))
+
+print(data)
