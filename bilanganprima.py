0bacdd647b12f3a874c97450efa01974c4923fb4
Novya Eka Ningroom Juana <99195707+novyaenj@users.noreply.github.com>
2022-02-07 22:02:09 +0700

    Update bilanganprima.py

--- bilanganprima.py
+++ bilanganprima.py
@@ -1,7 +1,7 @@
 from itertools import count
 
 
-def is_prima (x):
+def bilangan_prima (x):
     for i in range(2,x):
         if x % i == 0:
             return False
@@ -12,7 +12,7 @@ def cari_bilangan_prima(awal,akhir):
     list_bilangan_prima = []
 
     for x in range(awal,akhir + 1):
-        if is_prima(x):
+        if bilangan_prima(x):
             list_bilangan_prima.append(x)
     return list_bilangan_prima
 
